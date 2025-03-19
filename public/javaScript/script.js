fab8b8dbfe94b907f9e29898c3a96e12b876f559
Backspac3_N00B <leandro.oliveiracustodio02@gmail.com>
2025-03-19 11:59:43 -0300

    ajuste sessão de projetos

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -9,11 +9,15 @@ function loadProjects(data) {
         let html = '';
         data.forEach(element => {
             html += `
-            <div class="projects">
-                <div class="project">
+            <div class="project">
+                <div class="project_details">
                     <img src="${element.image}">
-                    <h1>${element.name}</h1>
-                    <p>${element.description}</p>
+                </div>
+                <div class="project_description">
+                    <div class="project_description_text">
+                        <h1>${element.name}</h1>
+                        <p>${element.description}</p>
+                    </div>
                     <a href="${element.link}" target="_blank"><button type="button" >Acessar projeto</button></a>
                 </div>
             </div>
@@ -24,34 +28,6 @@ function loadProjects(data) {
     })
 }
 
-
-
-function getProjects(){
-    let url = '/getprojects'
-
-    let xhttp = new XMLHttpRequest()
-    xhttp.open("get", url, false)
-    xhttp.send()
-
-    let data = JSON.parse(xhttp.responseText)
-
-    let html = '';
-        data.forEach(element => {
-            console.log(element)
-            html += `
-            <div class="projects">
-                <div class="project">
-                    <img src="${element.image}">
-                    <h1>${element.name}</h1>
-                    <p>${element.description}</p>
-                    <a href="${element.link}" target="_blank"><button type="button" >Acessar projeto</button></a>
-                </div>
-            </div>
-            `;
-
-        });
-        projects.innerHTML = html;
-}
 
 if (window.location.href == "http://localhost:3000/") {
     getProjects()
